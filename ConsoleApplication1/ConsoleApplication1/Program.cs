d2dea4f11fa32aef2e6fc993bda15252475d569f
yoss <yoss@DESKTOP-VPPC0E8>
2016-09-02 21:27:19 +0300

    a

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int fun_read()
+        {
+            ConsoleKeyInfo a = Console.ReadKey();
+            return  int.Parse(a.KeyChar.ToString());
+        }
+
+
+        static int fun_f(int num)
+        {
+           
+            if (num>0)
+                num *= fun_f(num - 1);
+           
+                return num;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("abc");
+            int num=fun_read();
+            //Console.WriteLine(num+5);
+            fun_f(num);
+            Console.WriteLine(num);
+
+            num = fun_read(); //hold screen
+        }
+    }
+}
